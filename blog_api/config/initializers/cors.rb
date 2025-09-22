852b164fa82ac5d4eace3a26d852e2ddffe72c79
Shreyaas14 <shreyaas.sureshbabu114@gmail.com>
2025-09-22 01:54:03 -0700

    Ad AiAuth middleware and initializer

--- blog_api/config/initializers/cors.rb
+++ blog_api/config/initializers/cors.rb
@@ -1,15 +1,15 @@
+# config/initializers/cors
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins(
       %r{\Ahttps://my-frontend-.*-shreyaas14s-projects\.vercel\.app\z},
-      "http://localhost:3000",
-      "https://my-frontend-ten-chi.vercel.app",
+      'http://localhost:3000',
+      'https://my-frontend-ten-chi.vercel.app'
     )
 
-    resource "*",
-      headers: :any,                               # <— allow any request headers
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      max_age: 600
-      # credentials: true  # only if you later use cookies; don't enable yet
+    resource '*',
+             headers: :any,
+             methods: %i[get post put patch delete options head],
+             max_age: 600
   end
 end
