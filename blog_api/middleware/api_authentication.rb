da36e940211cbc0ceae8aba7e394fcee0b014ee5
Shreyaas14 <shreyaas.sureshbabu114@gmail.com>
2025-09-22 00:12:09 -0700

    middleware module

--- blog_api/middleware/api_authentication.rb
+++ blog_api/middleware/api_authentication.rb
@@ -1,16 +1,18 @@
-class ApiAuthentication
-  def initialize(app)
-    @app = app
-  end
-
-  def call(env)
-    request = Rack::Request.new(env)
-    if request.path =~ %r{^/posts\.json$}
-      auth_token = env['HTTP_X_API_TOKEN']
-      unless auth_token == ENV['API_AUTH_TOKEN']
-        return [401, { 'Content-Type' => 'application/json' }, [{ error: 'Unauthorized' }.to_json]]
-      end
+module Middleware
+  class ApiAuthentication
+    def initialize(app)
+      @app = app
     end
-    @app.call(env)
+
+    def call(env)
+      request = Rack::Request.new(env)
+      if request.path =~ %r{^/posts\.json$}
+        auth_token = env['HTTP_X_API_TOKEN']
+        unless auth_token == ENV['API_AUTH_TOKEN']
+          return [401, { 'Content-Type' => 'application/json' }, [{ error: 'Unauthorized' }.to_json]]
+        end
+      end
+      @app.call(env)
+    end
   end
 end
