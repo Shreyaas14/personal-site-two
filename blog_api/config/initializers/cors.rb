6b598fba027898183eea3ae6b3140797c30b7bd4
Shreyaas14 <shreyaas.sureshbabu114@gmail.com>
2025-09-22 01:49:30 -0700

    Ad AiAuthn middleware and initializer

--- blog_api/config/initializers/cors.rb
+++ blog_api/config/initializers/cors.rb
@@ -1,9 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins \
+    origins(
       %r{\Ahttps://my-frontend-.*-shreyaas14s-projects\.vercel\.app\z},
       "http://localhost:3000",
       "https://my-frontend-ten-chi.vercel.app",
+    )
 
     resource "*",
       headers: :any,                               # <— allow any request headers
